db7ec5fc3a982a33081d821a2d2c9af1d45a8be2
George  Borsato <georginhoborsato@gmail.com>
2024-03-28 22:04:55 -0300

    feat: add tailwindcss and configuration the new source

--- App.tsx
+++ App.tsx
@@ -24,6 +24,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { MyText } from './src/components/MyText';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -77,7 +78,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text className="text-blue-500">App.tsx</Text> to change this
+            Edit <Text className="">App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
